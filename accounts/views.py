fb29aa15dc94c5299d88d80eacb14c282c27da6f
Eshank Vaish <eshank.vaish@gmail.com>
2020-04-04 02:33:49 +0530

    Login and Logout Complete

--- accounts/views.py
+++ accounts/views.py
@@ -4,13 +4,13 @@ from django.contrib import auth
 
 # Create your views here.
 def signup(request):
-    if request.method== 'POST':
+    if request.method=='POST':
         if request.POST['password']==request.POST['confirm-password']:
             try:
                 user = User.objects.get(username=request.POST['username'])
                 return render(request, 'accounts/signup.html', {'error':'Username already taken!!!'})
             except User.DoesNotExist:
-                user = User.objects.create_user(request.POST['username'],request.POST['password'])
+                user = User.objects.create_user(username=request.POST['username'],password=request.POST['password'])
                 auth.login(request,user)
                 return redirect('home')
         else:
@@ -19,9 +19,21 @@ def signup(request):
         return render(request, 'accounts/signup.html')
     
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method=='POST':
+        user = auth.authenticate(username=request.POST['username'],password=request.POST['password'])
+        if user:
+            auth.login(request,user)
+            return redirect('home')
+        else:
+            return render(request,'accounts/login.html',{'error':'username or password is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
     # TODO: need to route to homepage
     # Need to add logout
+    if request.method=='POST':
+        auth.logout(request)
+        return redirect('home')
+
     return render(request, 'accounts/logout.html')
